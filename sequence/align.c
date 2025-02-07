736719df72bd527ff03412a9a8e76dbf4492eb92
theUzumaki <mattiamaiorano02@gmail.com>
2025-02-07 14:13:50 +0000

    Succesful program cuda - 10k test gives 1.3 sec

--- sequence/align.c
+++ sequence/align.c
@@ -414,15 +414,6 @@ int main(int argc, char *argv[]) {
 	printf("-----------------\n");
 #endif // DEBUG
 
-printf("\nGLOBAL PAT FOUND: ");
-for (int i= 0; i < pat_number; i++) if (pat_found[i] != (unsigned long)NOT_FOUND) printf("%lu ", pat_found[i]);
-printf("\n\n");
-
-printf("GLOBAL SEQ MATCHES: ");
-for (int i= 0; i < seq_length; i++) if (seq_matches[i] != NOT_FOUND) printf("%d ", seq_matches[i]);
-printf("\n\n");
-
-
 	/* Free local resources */	
 	free( sequence );
 	free( seq_matches );
