34a52f239e6121feebcedb81a0fae57f5be6db20
Mattia Maiorano <mattiamaiorano02@gmail.com>
2025-04-05 13:59:55 +0200

    General update

--- push_git.sh
+++ push_git.sh
@@ -6,6 +6,8 @@ git add .
 # Commit with a message
 git commit -m "General update"
 
+REPO_URL="https://github.com/theUzumaki/WASA.git"
+
 # GitHub credentials
 GITHUB_USERNAME="theUzumaki"
 GITHUB_TOKEN="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"  # Preferably use a token instead of your password for security
