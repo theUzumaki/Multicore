6d31d872e6278efb881af4aafc4dd2e909eafada
Mattia Maiorano <mattiamaiorano02@gmail.com>
2025-04-05 13:59:20 +0200

    General update

--- push_git.sh
+++ push_git.sh
@@ -11,5 +11,5 @@ GITHUB_USERNAME="theUzumaki"
 GITHUB_TOKEN="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"  # Preferably use a token instead of your password for security
 
 # Push latest changes
-git push https://$GITHUB_USERNAME:$GITHUB_TOKEN@$REPO_URL
+git push https:/$GITHUB_USERNAME:$GITHUB_TOKEN@$REPO_URL
 
