fbee23bc176c3424232bf89d3a2cdc44514c71ab
Mattia Maiorano <mattiamaiorano02@gmail.com>
2025-04-12 21:45:49 +0200

    After failing lots of reduce i have tried with a multi kernel approach, no tests

--- sequence/cuda/cuda_mpi.cu
+++ sequence/cuda/cuda_mpi.cu
@@ -101,14 +101,15 @@ __global__ void search_patterns(char *d_sequence, char **d_pattern, unsigned lon
 __global__ void reduced_sum(int *d_block_pat_matches, int *d_total_matches, int length) {
 	int tid = threadIdx.x;
 	extern __shared__ int shared_data[];
-	int amount = length / blockDim.x;
-	for (int i = 0; i < amount; i++) shared_data[tid * amount + i] = d_block_pat_matches[tid * amount + i];
+	int amount = length / blockDim.x; 
+	for (int i = 0; i < amount; i++) if (tid * amount + i < length) shared_data[tid * amount + i] = d_block_pat_matches[tid * amount + i];
 	__syncthreads();
 
 	for (int stride = length / 2; stride > 0; stride /= 2) {
+		amount = amount / 2;
 		for (int i = 0; i < amount; i++) {
 			if (tid * amount + i < stride) {
-				shared_data[tid * amount + i] += shared_data[(tid * amount + stride) + i];
+				shared_data[tid * amount + i] += shared_data[tid * amount + stride + i];
 			}
 		}
 		__syncthreads();
