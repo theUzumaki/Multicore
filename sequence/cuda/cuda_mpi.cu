7da8998bacb65bebe7643d3ac8b218ba97623937
Mattia Maiorano <mattiamaiorano02@gmail.com>
2025-04-13 23:40:10 +0200

    After failing lots of reduce i have tried with a multi kernel approach, some tests still failed, debug mod, major mod

--- sequence/cuda/cuda_mpi.cu
+++ sequence/cuda/cuda_mpi.cu
@@ -143,13 +143,15 @@ __global__ void reduced_sum(int *d_block_pat_matches, int *d_total_matches, int 
     __syncthreads();
 
     // Perform binary tree reduction
-    for (int stride = (blockDim.x + 1) / 2; stride > 0; stride = (stride + 1) / 2) {
-        if (tid + stride < blockDim.x) {
+	int red_length = blockDim.x;
+    for (int stride = (red_length + 1) / 2; stride > 0; stride = (stride + 1) / 2) {
+        if (tid + stride < red_length) {
 			printf("shared_data[%d] = %d + %d\n", tid, shared_data[tid], shared_data[tid + stride]);
             shared_data[tid] += shared_data[tid + stride];
 			printf("shared_data[%d] = %d\n", tid, shared_data[tid]);
         }
         __syncthreads();
+		red_length = tid + stride
 		if (stride == 1) {
 			break;
 		}
