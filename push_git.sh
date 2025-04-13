18cafae5eccc889ab0dee573ab931aa5fccd7a8c
theUzumaki <mattiamaiorano02@gmail.com>
2025-04-13 16:27:14 +0000

    After failing lots of reduce i have tried with a multi kernel approach, some tests still failed, trying again

--- push_git.sh
+++ push_git.sh
@@ -4,7 +4,7 @@
 git add .
 
 # Commit with a message
-git commit -m "After failing lots of reduce i have tried with a multi kernel approach, some tests failed, trying again"
+git commit -m "After failing lots of reduce i have tried with a multi kernel approach, some tests still failed, trying again"
 
 REPO_URL="github.com/theUzumaki/Multicore.git"
 
