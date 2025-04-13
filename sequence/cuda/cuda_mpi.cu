e96fd3f5c5cd0d94402f82dc1c09f82794c038f3
Mattia Maiorano <mattiamaiorano02@gmail.com>
2025-04-13 19:41:00 +0200

    After failing lots of reduce i have tried with a multi kernel approach, some tests still failed, debug mod, major mod

--- sequence/cuda/cuda_mpi.cu
+++ sequence/cuda/cuda_mpi.cu
@@ -597,6 +597,17 @@ int main(int argc, char *argv[]) {
 	int threads_per_block_reduction = min(1024, blocks_per_grid);
 	int blocks_per_grid_reduction = (blocks_per_grid + threads_per_block_reduction - 1) / threads_per_block_reduction;
 
+	int *h_pat_matches = (int *)malloc(sizeof(int) * blocks_per_grid);
+	CUDA_CHECK_FUNCTION(cudaMemcpy(h_pat_matches, d_pat_matches, sizeof(int) * blocks_per_grid, cudaMemcpyDeviceToHost));
+
+	printf("d_pat_matches:\n");
+	for (int i = 0; i < blocks_per_grid; i++) {
+		printf("%d ", h_pat_matches[i]);
+	}
+	printf("\n");
+
+	free(h_pat_matches);
+
 	int *d_pat_reduction;
 	int length_pat_matches = blocks_per_grid;
 	while (blocks_per_grid_reduction > 1) {
@@ -609,6 +620,16 @@ int main(int argc, char *argv[]) {
 		// Exchanging old block reduction with new one
 		CUDA_CHECK_FUNCTION( cudaFree(d_pat_matches) );
 		d_pat_matches = d_pat_reduction;
+
+		CUDA_CHECK_FUNCTION(cudaMemcpy(h_pat_matches, d_pat_matches, sizeof(int) * blocks_per_grid, cudaMemcpyDeviceToHost));
+
+		printf("d_pat_matches:\n");
+		for (int i = 0; i < blocks_per_grid; i++) {
+			printf("%d ", h_pat_matches[i]);
+		}
+		printf("\n");
+	
+		free(h_pat_matches);
 	}
 
 	reduced_sum<<<blocks_per_grid_reduction, threads_per_block_reduction, threads_per_block_reduction * sizeof(int)>>>(d_pat_matches, d_total_matches, length_pat_matches);
