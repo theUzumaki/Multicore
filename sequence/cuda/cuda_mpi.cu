c485e77b0b228f193391008df7c60dfaa5049826
Mattia Maiorano <mattiamaiorano02@gmail.com>
2025-04-13 23:40:56 +0200

    After failing lots of reduce i have tried with a multi kernel approach, some tests still failed, debug mod, major mod

--- sequence/cuda/cuda_mpi.cu
+++ sequence/cuda/cuda_mpi.cu
@@ -151,7 +151,7 @@ __global__ void reduced_sum(int *d_block_pat_matches, int *d_total_matches, int 
 			printf("shared_data[%d] = %d\n", tid, shared_data[tid]);
         }
         __syncthreads();
-		red_length = tid + stride
+		red_length = tid + stride;
 		if (stride == 1) {
 			break;
 		}
