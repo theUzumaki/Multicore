7d65a0d8e81834e03b4ea043c9fc6744bc325f62
Mattia Maiorano <mattiamaiorano02@gmail.com>
2025-04-15 00:40:37 +0200

    After failing lots of reduce i have tried with a multi kernel approach, some tests still failed, debug mod seems SUCCESS

--- sequence/cuda/cuda_mpi.cu
+++ sequence/cuda/cuda_mpi.cu
@@ -123,9 +123,11 @@ __global__ void partial_reduce(int *d_block_pat_matches, int *d_total_matches, i
 	
 	__syncthreads();
 	printf("THREAD %d GLOBAL %d\n", tid, global_idx);
-	if (threadIdx.x == 0) printf("Thread %d, global_idx %d, upper_limit %d\n", tid, global_idx, upper_limit);
-	if (threadIdx.x >= upper_limit) return;
-	if (threadIdx.x == 0) printf("Thread %d, global_idx %d, upper_limit %d\n", tid, global_idx, upper_limit);
+	if (tid == 0) printf("Thread %d, global_idx %d, upper_limit %d\n", tid, global_idx, upper_limit);
+	if (global_idx == 0) printf("Thread %d, global_idx %d, upper_limit %d\n", tid, global_idx, upper_limit);
+	if (global_idx >= length) return;
+	if (tid == 0) printf("Thread %d, global_idx %d, upper_limit %d\n", tid, global_idx, upper_limit);
+	if (global_idx == 0) printf("Thread %d, global_idx %d, upper_limit %d\n", tid, global_idx, upper_limit);
 	// Perform binary tree reduction
 	int red_length = upper_limit;
 	if (tid == 0) printf("BLOCK NUMBER %d has red_length = %d\n", blockIdx.x, red_length);
