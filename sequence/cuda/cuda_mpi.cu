e45f1cc86fe0069c56d0ff4ca528eab7bd510188
Mattia Maiorano <mattiamaiorano02@gmail.com>
2025-04-15 00:06:40 +0200

    After failing lots of reduce i have tried with a multi kernel approach, some tests still failed, debug mod seems SUCCESS

--- sequence/cuda/cuda_mpi.cu
+++ sequence/cuda/cuda_mpi.cu
@@ -122,11 +122,13 @@ __global__ void partial_reduce(int *d_block_pat_matches, int *d_total_matches, i
 	}
 	
 	__syncthreads();
-	if (blockIdx.x == 0) {
-		printf("d_block_pat_matches[%d] = %d\n", global_idx, d_block_pat_matches[global_idx]);
+	if (tid == 0 && blockIdx.x == 0) {
+		printf("Thread %d, block %d, global_idx %d, upper_limit %d\n", tid, blockIdx.x, global_idx, upper_limit);
 	}
 	if (tid >= upper_limit) return;
-
+	if (tid == 0 && blockIdx.x == 0) {
+		printf("Thread %d, block %d, global_idx %d, upper_limit %d\n", tid, blockIdx.x, global_idx, upper_limit);
+	}
 	// Perform binary tree reduction
 	int red_length = upper_limit;
 	if (tid == 0) printf("BLOCK NUMBER %d has red_length = %d\n", blockIdx.x, red_length);
