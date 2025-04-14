19d7814d8e49c8d4a9e7f1fb96962f63929e6183
Mattia Maiorano <mattiamaiorano02@gmail.com>
2025-04-14 16:30:54 +0200

    After failing lots of reduce i have tried with a multi kernel approach, some tests still failed, debug mod seems SUCCESS

--- sequence/cuda/cuda_mpi.cu
+++ sequence/cuda/cuda_mpi.cu
@@ -169,7 +169,7 @@ __global__ void reduced_sum(int *d_block_pat_matches, int *d_total_matches, int 
 	// Write the result from thread 0 to global memory
 	if (tid == 0) {
 		printf("d_block_pat_matches[%d] = %d\n", blockIdx.x, shared_data[0]);
-		d_total_matches[blockDim.x] = shared_data[0];
+		d_total_matches[blockIdx.x] = shared_data[0];
 	}
 }
 
